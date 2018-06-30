0a6cc6b2c25819ce905f5d6646b2d7d44a02f139
Damian <surrerstry@gmail.com>
2018-06-30 12:34:52 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smart_input
 Library to filtering out user input.
+
+https://surrerstry.pl/smart_input
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+	name = 'smart_input',
+	packages = ['smart_input'],
+	version = '0.1',
+	description = "Library to filtering out user input.",
+	author = 'Damian Medrala',
+	author_email = 'surrerstry@gmail.com',
+	url = 'https://github.com/Surrerstry/smart_input',
+	download_url = 'https://github.com/Surrerstry/smart_input/archive/master.zip',
+	keywords = ['input', 'filtrate'],
+	classifiers = [],
+	long_description = '...',
+	)
