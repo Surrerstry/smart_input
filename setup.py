c196a9a6535b477ea5a70125b4fb3b283abbd37a
Damian <surrerstry@gmail.com>
2018-07-22 18:26:04 +0200

    added method .menu

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
 	name = 'smart_input',
 	packages = ['smart_input'],
-	version = '0.1',
+	version = '0.3',
 	description = "Library to filtering out user input.",
 	author = 'Damian Medrala',
 	author_email = 'surrerstry@gmail.com',
@@ -11,5 +11,5 @@ setup(
 	download_url = 'https://github.com/Surrerstry/smart_input/archive/master.zip',
 	keywords = ['input', 'filtrate'],
 	classifiers = [],
-	long_description = '...',
+	long_description = 'More informations: https://surrerstry.pl/smart_input',
 	)
